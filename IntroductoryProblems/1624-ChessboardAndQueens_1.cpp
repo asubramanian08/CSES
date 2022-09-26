fabaf32333b084f927f6c2672758317b9372e193
asubramanian08 <arjun.subramanian.08@gmail.com>
2022-09-26 18:24:12 -0400

    Practice: 1624 (IntroductoryProblems)

--- IntroductoryProblems/1624-ChessboardAndQueens_1.cpp
+++ IntroductoryProblems/1624-ChessboardAndQueens_1.cpp
@@ -1,15 +1,61 @@
-// 3:30 -
+// 3:30 - 3:59
 #include <iostream>
 using namespace std;
+
+char board[8][8];
+int ans = 0;
+
+bool inRange(int x) { return 0 <= x && x < 8; }
+void mini_updt(int x, int y, bool add, int queenNum)
+{
+    if (inRange(x) && inRange(y) &&
+        (board[x][y] == '.' || board[x][y] == '0' + queenNum))
+        board[x][y] = add ? '0' + queenNum : '.';
+}
+void update(int x, int y, bool add, int queenNum)
+{
+    board[x][y] = add ? 'Q' : '.';
+    for (int i = x + 1; i < 8; i++)
+    {
+        mini_updt(i, y, add, queenNum);
+        mini_updt(i, y + (i - x), add, queenNum);
+        mini_updt(i, y - (i - x), add, queenNum);
+    }
+}
+void recursive(int row)
+{
+    if (row == 8)
+    {
+        ans++;
+        return;
+    }
+    for (int i = 0; i < 8; i++)
+        if (board[row][i] == '.')
+        {
+            update(row, i, true, row);
+            recursive(row + 1);
+            update(row, i, false, row);
+        }
+}
+string displayboard()
+{
+    string str = "";
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+            str += board[i][j];
+        str += '\n';
+    }
+    return str;
+}
 
 int main(void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n;
-    cin >> n;
-    int ans = 0;
-    for (int i = 0; i < n; i++)
-        ans = (ans << 1) + 1;
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++)
+            cin >> board[i][j];
+    recursive(0);
     cout << ans << endl;
 }
--- IntroductoryProblems/1624-ChessboardAndQueens_2.cpp
+++ IntroductoryProblems/1624-ChessboardAndQueens_2.cpp
@@ -1,15 +1,35 @@
-// 3:30 -
+// 6:17 - 6:23
 #include <iostream>
 using namespace std;
+
+char board[8][8];
+bool col[8], diag1[15], diag2[15];
+int ans = 0;
+
+void backtracking(int row)
+{
+    if (row == 8)
+    {
+        ans++;
+        return;
+    }
+    for (int i = 0; i < 8; i++)
+        if (int d1 = row + i, d2 = row - i + 7;
+            board[row][i] == '.' && !col[i] && !diag1[d1] && !diag2[d2])
+        {
+            col[i] = diag1[d1] = diag2[d2] = true;
+            backtracking(row + 1);
+            col[i] = diag1[d1] = diag2[d2] = false;
+        }
+}
 
 int main(void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n;
-    cin >> n;
-    int ans = 0;
-    for (int i = 0; i < n; i++)
-        ans = (ans << 1) + 1;
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++)
+            cin >> board[i][j];
+    backtracking(0);
     cout << ans << endl;
 }
